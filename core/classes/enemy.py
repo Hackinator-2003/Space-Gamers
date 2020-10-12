cdaebe0e1118705fca08398644ee00cdda465d77
Marc GUILLEMOT <mguillem@st-dominique.local>
2020-10-12 11:49:39 +0200

    update

--- core/classes/enemy.py
+++ core/classes/enemy.py
@@ -5,3 +5,4 @@ class enemy():
 
     def __init__(self):
         self.pos=position
+        self.Pv=Pv_monstre
--- core/classes/player.py
+++ core/classes/player.py
@@ -4,4 +4,5 @@ class player():
 
     def __init__(self):
         self.pos=position
+        self.Pv=3
 
