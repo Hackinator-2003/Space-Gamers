07b67f82de6022ca529ba2c322b3c74d460835d5
Cypooos <60020572+Cypooos@users.noreply.github.com>
2020-10-12 10:07:50 +0200

    configParser start

---
+++ core/configparser.py
@@ -0,0 +1,11 @@
+# By Cypooos
+
+class ConfigParser():
+
+
+    def __init__(self,filePath="conf.ini",defaultFile="core/default.ini"):
+        self.filePath = filePath
+        self.fileDefault = defaultFile
+
+        
+
