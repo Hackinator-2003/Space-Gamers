9781e70936ca1756a85a7986b98011fa3cbe7732
Cypooos <60020572+Cypooos@users.noreply.github.com>
2020-10-14 12:06:56 +0200

    log small update

--- core/game.py
+++ core/game.py
@@ -12,7 +12,7 @@ class Game():
     def set_up_logging(self):
         logging.basicConfig(level=logging.DEBUG)
         outFormatter = logging.Formatter("[%(levelname)-5.5s] %(message)s")
-        fileFormatter = logging.Formatter("%(asctime)s <%(filename)s:%(lineno)s - %(funcName)s()> [%(levelname)-5.5s] %(message)s")
+        fileFormatter = logging.Formatter("%(asctime)s <%(filename)s:%(lineno)s - %(funcName)10s()> [%(levelname)-5.5s] %(message)s")
         rootLogger = logging.getLogger()
         rootLogger.handlers = []
 
