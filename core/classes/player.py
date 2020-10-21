8acc8c786855bb4f843df5bba64ed45df37d0c53
marcg-92250 <71487632+marcg-92250@users.noreply.github.com>
2020-10-21 12:07:29 +0200

    update

--- core/classes/player.py
+++ core/classes/player.py
@@ -29,14 +29,14 @@ class Player():
         if self.pos[0]>550-60: self.pos[0]=550-60
 
     # Réaction après l'event de clic droit
-    def up(self,pos,dt):
+    def up(self,dt):
         logging.debug("Left click event called")
         self.pos[1]-=self.speed*dt
         if self.pos[1]<0: self.pos[1]=0
 
 
     # Réaction après l'event de clic droit
-    def down(self,pos,dt):
+    def down(self,dt):
         logging.debug("Right click event called")
         self.pos[1]+=self.speed*dt
         if self.pos[1]>700-60: self.pos[1]=700-60
