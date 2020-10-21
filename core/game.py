4df5502ea4c3d4945d714a2bfb815936c1300b79
Hackinator-2003 <Hackinator-2003@users.noreply.github.com>
2020-10-21 11:22:01 +0200

    Update game.py

--- core/game.py
+++ core/game.py
@@ -9,7 +9,7 @@ class Game():
         logging.debug("init game...")
         logging.debug("loading config...")
         self.config = get_config()
-        self.player = Player((225,500),3)
+        self.player = Player((252,500))
         self.enemys = [Enemy((30,30)),Enemy((60,30)),Enemy((90,30))]
 
     def start(self):
