da98eae72bd897a2e9c9907b6c6852e9db2a03a1
marcg-92250 <71487632+marcg-92250@users.noreply.github.com>
2020-10-27 13:36:03 +0100

    update boss et enemy

--- core/classes/bullet.py
+++ core/classes/bullet.py
@@ -7,10 +7,11 @@ class Bullet():
         self.pos=position
         self.handler = handler # à qui appratiens le rpojectile (au joueur ou aux ennemys)
         self.type_ = type_ # type of the projectile (mouvement différents etc)
+        self.speed = speed
 
-    def move(self,dt): 
-        if type_ == "up":self.pos[1] += speed*dt
-        if type_ == "down":self.pos[1] -= speed*dt
+    def move(self,dt):
+        if self.type_ == "up":self.pos[1] -= self.speed*dt
+        if self.type_ == "down":self.pos[1] += self.speed*dt
 
     def update(self,dt):
         pass
--- core/classes/enemy.py
+++ core/classes/enemy.py
@@ -4,6 +4,7 @@ class Enemy():
 
     nom = "Enemy"
     start_pv=(1,10)
+    fire_recovery = 5.5
 
 
     def __init__(self,game,position,pv=None):
@@ -12,13 +13,18 @@ class Enemy():
         if pv==None: self.pv=randint(self.start_pv[0],self.start_pv[1])
         else: self.pv = pv
         logging.debug("init enemys at "+str(position)+", pv="+str(self.pv))
+        self.lasershot_sound = pygame.mixer.Sound('core/rsc/sounds/laser_shot.wav')
+        self.fire_timer = 0
 
-    def update(self,dt):pass
+    def update(self,dt):
+        self.fire_timer += dt
 
 
 
     def shoot(self,dt):
         if self.fire_timer >= self.fire_recovery:
             self.lasershot_sound.play()
-            self.game.bullets.append(Bullet(self,[self.game.player.pos[0]+12,self.game.player.pos[1]-30],"down",100))
+            self.game.bullets.append(Bullet(self,[self.pos[0]+12,self.pos[1]-30],"down",100))
             self.fire_timer = 0.0
+
+
